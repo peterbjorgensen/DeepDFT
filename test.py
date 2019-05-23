c7d978569778b156b17e96b93b32f6ed4488975c
Peter Bjørn Jørgensen <peterbjorgensen@gmail.com>
2019-05-23 16:03:07 +0200

    fixed cumsum

--- test.py
+++ test.py
@@ -158,7 +158,8 @@ class ReadoutLastnode(msgnet.readout.ReadoutFunction):
 
     def __call__(self, nodes, segments):
         nodes_size = int(nodes.get_shape()[1])
-        last_node_idx = tf.segment_sum(tf.ones_like(segments), segments, name="set_len") - 1
+        set_len = tf.segment_sum(tf.ones_like(segments), segments, name="set_len") - 1
+        last_node_idx = tf.cumsum(set_len)
         last_nodes = tf.gather(nodes, last_node_idx)
         graph_out = msgnet.defaults.mlp(
             last_nodes,
@@ -176,6 +177,7 @@ def main():
     embedding_size = 10
 
     datawrapper = DataWrapper(vasp_charge, batch_size)
+    breakpoint()
 
     model = msgnet.MsgpassingNetwork(
         embedding_shape=(len(ase.data.chemical_symbols), embedding_size),
